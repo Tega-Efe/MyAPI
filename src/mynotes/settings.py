8edf4f04be258cfdf80d86ab134c483c9ca13a70
Tega-Efe <dominicadjeh@gmail.com>
2024-05-27 12:44:52 +0100

    updated

--- src/mynotes/settings.py
+++ src/mynotes/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a#!t!!0$58&ua-u)m9bzun9wugrofm3%3+#zop9-!7e)cuo0%k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1' ,'myapi-x4jf.onrender.com']
 
 
 # Application definition
